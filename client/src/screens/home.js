a0c26d77c194d8f0f33fa37f0a911653f8043d5a
vukhanh09 <vukhanh0920@gmail.com>
2022-05-27 01:16:37 +0700

    update product category

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -1,13 +1,28 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
+import styles from './CSS/home.module.css';
+import ProductCategory from '../components/productCategory';
+import BestSell from '../components/bestSell';
+
+
+
 
 
 function Home({navigation}) {
     return (
-      <div className="Home">
+      <div className={styles.Home}>
         <Header navigation={navigation}/>
-        <div>
-            Hello world
+        <div className={styles.content} >
+          <div>
+            <strong className={styles.tdisplay}>Danh mục</strong>
+            <ProductCategory/>
+          </div>
+
+          <div>
+            <strong className={styles.tdisplay}>Sản phẩm bán chạy</strong>
+            <BestSell/>
+          </div>
+           
         </div>
         <Footer navigation={navigation}/>
         
