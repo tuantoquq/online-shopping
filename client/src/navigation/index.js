18d43db0649372f5eb50dc3df27128ea91327e41
chimeyrock <trinhvanthoai99@gmail.com>
2022-06-02 16:07:16 +0700

    fix conflict merge request and add route

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,15 +1,22 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "../screens/home";
-import CommentProduct from "../screens/commentProduct";
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../screens/home';
+import CommentProduct from '../screens/commentProduct';
+import Login from '../screens/login';
+import Register from '../screens/register';
 
-function RootRoutes(){
-    return(
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/comment" element={<CommentProduct />} />
-      </Routes>
-    )
+function RootRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/comment" element={<CommentProduct />} />
+      <Route path="/customer/login" element={<Login role="customer" />} />
+      <Route path="/shopper/login" element={<Login role="shopper" />} />
+      <Route path="/admin/login" element={<Login role="admin" />} />
+      <Route path="/customer/register" element={<Register role="customer" />} />
+      <Route path="/shopper/register" element={<Register role="shopper" />} />
+      <Route path="/admin/register" element={<Register role="admin" />} />
+    </Routes>
+  );
 }
 
-
-export default RootRoutes
+export default RootRoutes;
