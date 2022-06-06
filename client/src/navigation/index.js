61870a8660c444d14c36535019e5b7e2ff44c581
Bùi Minh Tuấn <71689141+tuanbm00@users.noreply.github.com>
2022-06-07 01:17:35 +0700

    + demo chi tiet san pham

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,12 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../screens/home";
 import CommentProduct from "../screens/commentProduct";
+import ProductInformation from "../components/product";
 
 function RootRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/comment" element={<CommentProduct />} />
+            <Route path="/ProductTest" element = {<ProductInformation />} />
       </Routes>
     )
 }
