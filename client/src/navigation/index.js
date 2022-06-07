26dcced6649d7eac843971af115c2e5b57649356
chimeyrock <trinhvanthoai99@gmail.com>
2022-06-08 00:31:10 +0700

    completed search-UI

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
 import { Routes, Route, Link } from 'react-router-dom';
+
 import UserInformation from '../screens/UserInformation';
 import OrderHistory from '../screens/OrderHistory';
 import Order from '../screens/Cart';
-=======
-import { Routes, Route, Link } from "react-router-dom";
-
-import UserInformation from "../screens/UserInformation";
-import OrderHistory from "../screens/OrderHistory";
-import Order from "../screens/Cart";
->>>>>>> 76ed5aeb968347984811064e8d8dea452f7dc6d5
 import Home from '../screens/home';
 import CommentProduct from '../screens/commentProduct';
 import Login from '../screens/login';
 import Register from '../screens/register';
-<<<<<<< HEAD
 import Cart from '../screens/Cart';
 import Search from '../screens/search';
-=======
-import Cart from "../screens/Cart";
-import AcceptOrder from "../screens/seller_acceptOrder";
-import DeliveringOrder from "../screens/seller_deliveringOrder";
-import RejectOrder from "../screens/seller_rejectOrder";
-import DeliveredOrder from "../screens/seller_deliveredOrder";
->>>>>>> 76ed5aeb968347984811064e8d8dea452f7dc6d5
+import AcceptOrder from '../screens/seller_acceptOrder';
+import DeliveringOrder from '../screens/seller_deliveringOrder';
+import RejectOrder from '../screens/seller_rejectOrder';
+import DeliveredOrder from '../screens/seller_deliveredOrder';
 
 function RootRoutes() {
   return (
@@ -35,7 +24,6 @@ function RootRoutes() {
       <Route path="/shopper/delivering-order" element={<DeliveringOrder />} />
       <Route path="/shopper/reject-order" element={<RejectOrder />} />
       <Route path="/shopper/delivered-order" element={<DeliveredOrder />} />
-
 
       <Route path="/customer/login" element={<Login role="customer" />} />
       <Route path="/shopper/login" element={<Login role="shopper" />} />
@@ -49,7 +37,6 @@ function RootRoutes() {
       <Route path="/cart" element={<Cart />} />
     </Routes>
   );
-
 }
 
 export default RootRoutes;
