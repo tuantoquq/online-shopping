05d5aa6e9104e8256195bea338a130ed1c515cd5
Tuan Nguyen <tuantoquq@gmail.com>
2022-06-15 08:54:01 +0700

    back-03: fix routes format error

--- server/src/routes/delivery.routes.js
+++ server/src/routes/delivery.routes.js
@@ -1,27 +1,27 @@
 import express from 'express';
-import deliveryControler from '../controller/deliveryAddress.js';
+import deliveryController from '../controller/deliveryAddress.js';
 import { verifyToken } from '../middleware/authJwt.js';
 
 const deliverRoutes = express.Router();
 deliverRoutes.post(
     '/api/v1/delivery/insert',
     verifyToken,
-    deliveryControler.insertToDatabase,
+    deliveryController.insertToDatabase,
 );
 deliverRoutes.get(
     '/api/v1/delivery/get',
     verifyToken,
-    deliveryControler.getDeliveryFromDatabase,
+    deliveryController.getDeliveryFromDatabase,
 );
 deliverRoutes.post(
     '/api/v1/delivery/delete',
     verifyToken,
-    deliveryControler.deleteDeliveryFromDatabse,
+    deliveryController.deleteDeliveryFromDatabase,
 );
 deliverRoutes.post(
     '/api/v1/delivery/update',
     verifyToken,
-    deliveryControler.updateDeliveryFromDatabase,
+    deliveryController.updateDeliveryFromDatabase,
 );
 
 export default deliverRoutes;
