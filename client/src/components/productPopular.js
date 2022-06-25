5fa553870157f128024b85b37888a737df054a99
vukhanh09 <vukhanh0920@gmail.com>
2022-06-25 23:57:20 +0700

    add

--- client/src/components/productPopular.js
+++ client/src/components/productPopular.js
@@ -12,66 +12,53 @@ import axiosConfig from '../config/axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function ProductPopular(){
-    
-    const [productData,setProductData] = useState()
-
-    useEffect(()=>{
-      axiosConfig.get('/product/top-6-selling').then(res=>{
-        setProductData(res.data.data)
-      })
-      .catch(err=>{
-        console.log(err)
-      })
-
-    },[])
-    console.log(productData);
-    return (
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        style={{ padding: "10px" }}
-      >
-        {productData?.map((product, index) => {
-          let product_name = product?.productName
-            .split(" ")
-            .slice(0, 5)
-            .join(" ");
-          return (
-            <Grid item xs={2} sm={1} md={2} key={index}>
-              <Link
-                to={"/ProductTest/" + product?._id}
-                style={{ "text-decoration": "none" }}
-              >
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="100"
-                      image={product?.imageUrls[0]?.base_url}
-                      alt="green iguana"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="p" component="div">
-                        {product_name}
-                      </Typography>
-                      <Typography
-                        gutterBottom
-                        variant="p"
-                        component="div"
-                        color="red"
-                      >
-                        đ {product?.price}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Link>
-            </Grid>
-          );
-        })}
-      </Grid>
-    );
+function ProductPopular({ productPopular }) {
+  return (
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+      style={{ padding: "10px" }}
+    >
+      {productPopular?.map((product, index) => {
+        let product_name = product?.productName
+          .split(" ")
+          .slice(0, 5)
+          .join(" ");
+        return (
+          <Grid item xs={2} sm={1} md={2} key={index}>
+            <Link
+              to={"/ProductTest/" + product?._id}
+              style={{ "text-decoration": "none" }}
+            >
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="100"
+                    image={product?.imageUrls[0]?.base_url}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="p" component="div">
+                      {product_name}
+                    </Typography>
+                    <Typography
+                      gutterBottom
+                      variant="p"
+                      component="div"
+                      color="red"
+                    >
+                      đ {product?.price}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 export default ProductPopular
