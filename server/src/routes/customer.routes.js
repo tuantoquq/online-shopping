abb731736c6ccaaed23d603d93f612fbc4a19bcc
Bùi Minh Tuấn <71689141+tuanbm00@users.noreply.github.com>
2022-06-26 22:57:59 +0700

    chuyen tab product manager

--- server/src/routes/customer.routes.js
+++ server/src/routes/customer.routes.js
@@ -2,19 +2,10 @@ import express from 'express';
 import { cancelOrder, getCustomerProfile } from '../controller/customer.controller.js';
 import { updateInforCustomer } from '../controller/auth.controller.js';
 import { verifyToken } from '../middleware/authJwt.js';
-import { updateInforCustomer } from '../controller/auth.js';
 
 const customerRoutes = express.Router();
 
 customerRoutes.get('/api/v1/customer/auth/get-profiles', verifyToken, getCustomerProfile);
 customerRoutes.post('/api/v1/customer/auth/cancel-order', verifyToken, cancelOrder);
-<<<<<<< HEAD
-<<<<<<< HEAD
 customerRoutes.post('/api/v1/customer/auth/update', verifyToken, updateInforCustomer);
-=======
-customerRoutes.post('/api/v1/customer/update', verifyToken, updateInforCustomer)
->>>>>>> fix search engine
-=======
-customerRoutes.post('/api/v1/customer/update', verifyToken, updateInforCustomer)
->>>>>>> fix search engine
 export default customerRoutes;
