1aaf4ef910ecfe47f893bd7e1fa6fc87b07b6146
daominhkhanh20 <khanhc1k36@gmail.com>
2022-07-02 13:31:18 +0700

    update

--- server/src/routes/customer.routes.js
+++ server/src/routes/customer.routes.js
@@ -3,6 +3,7 @@ import {
     cancelOrder,
     getCustomerProfile,
     updateAvatar,
+    getAllCustomer
 } from '../controller/customer.controller.js';
 import { updateInforCustomer } from '../controller/auth.controller.js';
 import { verifyToken } from '../middleware/authJwt.js';
@@ -17,6 +18,7 @@ customerRoutes.get('/api/v1/customer/auth/get-profiles', verifyToken, getCustome
 customerRoutes.post('/api/v1/customer/auth/cancel-order', verifyToken, cancelOrder);
 customerRoutes.post('/api/v1/customer/auth/update', verifyToken, updateInforCustomer);
 customerRoutes.get('/api/v1/customer/shop-info', getShopInfo);
+customerRoutes.get('/api/v1/customer/getall', getAllCustomer)
 customerRoutes.put(
     '/api/v1/customer/auth/update-avatar',
     [upload.single('file'), verifyToken],
