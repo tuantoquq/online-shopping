9c2eba8153a8f90a759154f113f5760f5f205caa
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-05 10:36:19 +0700

    back-07: add image to comment

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { addComment, getAllCommentOfProduct } from '../controller/comment.controller.js';
 import { verifyToken } from '../middleware/authJwt.js';
+import multer from 'multer';
+import { storage } from '../middleware/uploadFile.js';
 
 const commentRoutes = express.Router();
+const upload = multer({ storage: storage });
 
-commentRoutes.post('/api/v1/customer/auth/comments', verifyToken, addComment);
+commentRoutes.post('/api/v1/customer/auth/comments', [upload.array('files'),verifyToken], addComment);
 commentRoutes.get('/api/v1/comments', getAllCommentOfProduct);
 
 export default commentRoutes;
