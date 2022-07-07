402b424860fca7cada5a30571862a5ef1211ad53
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-07 20:33:07 +0700

    fe-merge-api: fix refresh token APi

--- client/src/service/ShopperApi.js
+++ client/src/service/ShopperApi.js
@@ -37,7 +37,7 @@ instance.interceptors.response.use(
         originalConfig._retry = true;
         try {
           const rs = await axios.post(
-            BASE_API_URL + '/shopper/refresh-token',
+            BASE_API_URL + '/shopper/refresh-token',{},
             {
               headers: { 
                 'Content-Type': 'application/json',
@@ -45,12 +45,12 @@ instance.interceptors.response.use(
               },
             }
           );
-          if (rs.data.status === 0) {
-            const accessToken = rs.data.data.token;
-            const refreshToken = rs.data.data.refreshToken;
+          if (rs?.data?.status === 0) {
+            const accessToken = rs?.data?.data?.token;
+            const refreshToken = rs?.data?.data?.refreshToken;
             TokenService.updateLocalAccessToken(
               ROLE.shopper,
-              `Bearer ${accessToken}`
+              accessToken
             );
             TokenService.updateLocalRefreshToken(ROLE.shopper, refreshToken);
           } else {
