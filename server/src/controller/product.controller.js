5220774b7afcd0a7650d7a03256241669ead11a8
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-21 21:30:11 +0700

    add check file type before upload

--- server/src/controller/product.controller.js
+++ server/src/controller/product.controller.js
@@ -281,6 +281,12 @@ export const addProduct = async (req, res, next) => {
         }
         //upload images
         let imageFiles = req.files;
+        if(!imageFiles.mimetype.startsWith("image/")){
+            return res.status(httpStatus.BAD_REQUEST).send({
+                status: apiStatus.INVALID_PARAM,
+                message: "Only support image file!"
+            });
+        }
         //check file extensions
         if (!imageFiles) {
             const error = new Error('Upload file again!');
