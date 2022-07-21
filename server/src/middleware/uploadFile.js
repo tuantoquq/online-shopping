f5b29fb78c77b0d216d0d968115ed61f1147872d
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-21 11:10:01 +0700

    back-fix: continue format file upload name

--- server/src/middleware/uploadFile.js
+++ server/src/middleware/uploadFile.js
@@ -7,6 +7,6 @@ export const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         const fileName = Date.now() + Math.round(Math.random() * 1e9);
         // eslint-disable-next-line no-useless-escape
-        cb(null, fileName + '-' + file.originalname.trim().replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, ''));
+        cb(null, fileName + '-' + file.originalname.trim().replace(/[&\/\\#, +()$~%'":*?<>{}]/g, ''));
     },
 });
