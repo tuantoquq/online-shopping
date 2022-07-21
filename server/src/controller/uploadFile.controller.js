e647dd050dc02068c9512fde67ad3327e500fc97
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-21 10:29:57 +0700

    back-fix: add check type of file upload and bad comment

--- server/src/controller/uploadFile.controller.js
+++ server/src/controller/uploadFile.controller.js
@@ -3,6 +3,13 @@ import { httpStatus, apiStatus } from '../constants/index.js';
 export const uploadFile = async (req, res, next) => {
     const file = req.file;
 
+    if(!file.mimetype.startsWith("image/")){
+        return res.status(httpStatus.BAD_REQUEST).send({
+            status: apiStatus.INVALID_PARAM,
+            message: "Only support image file!"
+        });
+    }
+
     //check file extensions
     if (!file) {
         const error = new Error('Upload file again!');
