328a7fed9879bd9bac5e3412165a4b8d5f514644
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-23 17:57:12 +0700

    back-fix: add statistic number access

--- server/src/routes/statistic.routes.js
+++ server/src/routes/statistic.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import statisticController from '../controller/statistic.controller.js'
+import { getNumberAccess } from '../controller/auth.controller.js'
 
 const statisticRoutes = express.Router()
 
@@ -10,7 +11,7 @@ statisticRoutes.post('/api/v1/statistic/countCustomer', statisticController.coun
 statisticRoutes.post('/api/v1/statistic/countRevenue', statisticController.countRevenue)
 statisticRoutes.post("/api/v1/statistic/top-category", statisticController.countTopEachCategory)
 statisticRoutes.post("/api/v1/statistic/count-cancel-order", statisticController.countCancelOrder)
-
+statisticRoutes.post('/api/v1/statistic/get-access', getNumberAccess);
 
 
 
