cd47dab9619acc2ac8bd08bbe27965820b77e606
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-25 14:14:08 +0700

    back-build: build v3.2

--- client/src/constants/Constants.js
+++ client/src/constants/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://localhost:8081/api/v1';
+export const BASE_API_URL = 'http://tipi-shopping.eastasia.cloudapp.azure.com/api/v1';
 export const ROLE = {
     customer: 'customer',
     shopper: 'shopper',
