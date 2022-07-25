f42882a0be2a9c786af29ae5f5290a3d54cf65ff
Tuan Nguyen <tuantoquq@gmail.com>
2022-07-25 16:42:52 +0700

    back-fix: add change password API for admin

--- server/src/controller/admin.controller.js
+++ server/src/controller/admin.controller.js
@@ -2,6 +2,10 @@ import CustomError from "../error/customError.js";
 import { httpStatus, apiStatus, RoleConstants } from "../constants/index.js";
 import CustomerService from "../service/customer.service.js";
 import ShopperService from "../service/shopper.service.js";
+import AdminService from "../service/admin.service.js";
+import bcrypt from 'bcryptjs';
+
+const {hashSync, compareSync} = bcrypt; 
 
 export const onOffBlockUser = async (req, res) => {
     try{
@@ -88,6 +92,48 @@ export const changeRequestShopperStatus = async (req, res) => {
             message: "Change shopper state successfully",
             data: updateShopper
         });
+    }catch(err){
+        if (err instanceof CustomError) {
+            return res.status(err.httpStatus).send({
+                status: err.apiStatus,
+                message: err.message,
+            });
+        }
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+            status: apiStatus.OTHER_ERROR,
+            message: err.message,
+        });
+    }
+}
+
+export const changePassword = async (req, res) => {
+    try{
+        let userId = req.userId;
+
+        let admin = await AdminService.findAdminById(userId);
+
+        let {oldPass, newPass} = req.body;
+
+        //check before change password
+        if(oldPass === newPass){
+            return res.status(httpStatus.BAD_REQUEST).send({
+                status: apiStatus.INVALID_PARAM,
+                message: "New password must be different with old password",
+            });
+        }
+
+        if(compareSync(oldPass, admin.password)){
+            await AdminService.updatePassword(userId, hashSync(newPass));
+            return res.status(httpStatus.OK).send({
+                status: apiStatus.SUCCESS,
+                message: "Change password successfully"
+            });
+        }else {
+            return res.status(httpStatus.BAD_REQUEST).send({
+                status: apiStatus.INVALID_PARAM,
+                message: "Old password not match!"
+            });
+        }
     }catch(err){
         if (err instanceof CustomError) {
             return res.status(err.httpStatus).send({
